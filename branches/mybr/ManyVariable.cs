21da95c17d361601f0e0307725b75ec5268eb3f4
plastiv <plastiv@aaa7b191-0527-1445-9cc7-a696b8423fba>
2009-12-19 21:31:34 +0000

    Добавил метод ля расширенного многогранника

--- branches/mybr/ManyVariable.cs
+++ branches/mybr/ManyVariable.cs
@@ -74,6 +74,19 @@ namespace OptimizationMethods
         }
 
         /// <summary>
+        /// Нахождение безусловного минимума функции многих переменных методом деформируемого многогранника.
+        /// </summary>
+        /// <param name="function">The function.</param>
+        /// <param name="dimension">The dimension.</param>
+        /// <param name="startingPoint">The starting point.</param>
+        /// <returns>Минимум функции.</returns>
+        public static double[][][] DeformablePolyhedronExtended(ManyVariable function, int dimension, double[] startingPoint)
+        {
+            DeformablePolyhedron dp = new DeformablePolyhedron(function, dimension);
+            return dp.GetExtendedMinimum(startingPoint, Precision);
+        }
+
+        /// <summary>
         /// Нахождение безусловного минимума функции многих переменных методом Хука-Дживса.
         /// </summary>
         /// <param name="function">The function.</param>

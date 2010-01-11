6ff89ed30a090717242fcf2f2efbaf56fb710007
plastiv <plastiv@aaa7b191-0527-1445-9cc7-a696b8423fba>
2010-01-11 21:48:02 +0000

    Создал первый тест, решающий пример с ответом 2,697.

--- Optimization/Optimization.Tests/TestFibonacciMethod.cs
+++ Optimization/Optimization.Tests/TestFibonacciMethod.cs
@@ -1,7 +1,24 @@
 ﻿
 namespace Optimization.Tests
 {
+    using NUnit.Framework;
+    using Optimization.Methods.ZerothOrder.OneVariable;
+
+    [TestFixture]
     class TestFibonacciMethod
     {
+        [Test]
+        public void TestMethodWork1()
+        {
+            OneVariableFunction ovf = delegate(double x)
+            {
+                return (2 * x * x - 12 * x);
+            };
+            double a0 = 0;
+            double b0 = 10;
+
+            Assert.AreEqual(2.697,Fibonacci.GetMinimum(ovf, a0, b0, 0.01),0.01);
+        }
+
     }
 }

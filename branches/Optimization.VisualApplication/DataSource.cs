878f63db91cd8a5fe95cc124989f531391bbfeb8
plastiv <plastiv@aaa7b191-0527-1445-9cc7-a696b8423fba>
2010-01-12 15:45:26 +0000

    Ответвление, в котором будет реализована линия вместе с маркерами на ней.

---
+++ branches/Optimization.VisualApplication/DataSource.cs
@@ -0,0 +1,164 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataSource.cs" company="Home Corporation">
+//     Copyright (c) Home Corporation 2009. All rights reserved.
+// </copyright>
+// <author>Sergii Pechenizkyi</author>
+//-----------------------------------------------------------------------
+
+namespace Optimization.VisualApplication
+{
+    using System.Windows;
+    using Microsoft.Research.DynamicDataDisplay.DataSources.MultiDimensional;
+    using OptimizationMethods;
+
+    /// <summary>
+    /// Класс, отвечающий за построение значений, для линии уровня функции (массив значений f(x1,x2) и сами значения x1,x2).
+    /// </summary>
+    internal static class DataSource
+    {
+        #region Public Fields
+
+        #endregion
+
+        #region Private Fields
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Gets the data source.
+        /// </summary>
+        /// <param name="func">Функция двух переменных.</param>
+        /// <param name="minValue">Минимальное значение x1,x2.</param>
+        /// <param name="maxValue">Максимальное значение x1,x2.</param>
+        /// <param name="pointCount">Количество расчитываемых точек в диапазоне |xmax-xmin|.</param>
+        /// <returns>Значения f(x1,x2), x1, x2.</returns>
+        internal static WarpedDataSource2D<double> GetDataSource(ManyVariable func, int minValue, int maxValue, int pointCount)
+        {
+            return new WarpedDataSource2D<double>(
+                GetData(func, minValue, maxValue, pointCount),
+                GetGridData(minValue, maxValue, pointCount));
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Gets сетку точек x1,x2 для которых будет построена линия уровня.
+        /// </summary>
+        /// <param name="minX">Минимальное значение x1, с которого начинать.</param>
+        /// <param name="maxX">Максимальное значение x1, которым заканчивать.</param>
+        /// <param name="minY">Минимальное значение x2, с которого начинать.</param>
+        /// <param name="maxY">Максимальное значение x2, которым заканчивать</param>
+        /// <param name="pointCountX">Количество расчитываемых точек в диапазоне |x1max-x1min|.</param>
+        /// <param name="pointCountY">Количество расчитываемых точек в диапазоне |x2max-x2min|.</param>
+        /// <returns>Точки[x1, x2].</returns>
+        private static Point[,] GetGridData(int minX, int maxX, int minY, int maxY, int pointCountX, int pointCountY)
+        {
+            double stepX = (double)(maxX - minX) / pointCountX;
+            double stepY = (double)(maxY - minY) / pointCountY;
+
+            double[] pointX = new double[pointCountX];
+            double[] pointY = new double[pointCountY];
+            for (int row = 0; row < pointCountX; row++)
+            {
+                pointX[row] = minX + (row * stepX);
+            }
+
+            for (int column = 0; column < pointCountY; column++)
+            {
+                pointY[column] = minY + (column * stepY);
+            }
+
+            Point[,] gridData = new Point[pointCountY, pointCountX];
+            for (int row = 0; row < pointCountX; row++)
+            {
+                for (int column = 0; column < pointCountY; column++)
+                {
+                    gridData[column, row] = new Point(pointY[column], pointX[row]);
+                }
+            }
+
+            return gridData;
+        }
+
+        /// <summary>
+        /// Gets сетку точек x1,x2 для которых будет построена линия уровня.
+        /// </summary>
+        /// <param name="minValue">Минимальное значение x1,x2.</param>
+        /// <param name="maxValue">Максимальное значение x1,x2.</param>
+        /// <param name="pointCount">Количество расчитываемых точек в диапазоне |xmax-xmin|.</param>
+        /// <returns>Точки[x1, x2].</returns>
+        private static Point[,] GetGridData(int minValue, int maxValue, int pointCount)
+        {
+            return GetGridData(minValue, maxValue, minValue, maxValue, pointCount, pointCount);
+        }
+
+        // ------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Gets значение функции в точках x1,x2 для которых будет построена линия уровня.
+        /// </summary>
+        /// <param name="func">Функция двух переменных.</param>
+        /// <param name="minX">Минимальное значение x1, с которого начинать.</param>
+        /// <param name="maxX">Максимальное значение x1, которым заканчивать.</param>
+        /// <param name="minY">Минимальное значение x2, с которого начинать.</param>
+        /// <param name="maxY">Максимальное значение x2, которым заканчивать</param>
+        /// <param name="pointCountX">Количество расчитываемых точек в диапазоне |x1max-x1min|.</param>
+        /// <param name="pointCountY">Количество расчитываемых точек в диапазоне |x2max-x2min|.</param>
+        /// <returns>Значение функции в точках x1,x2.</returns>
+        private static double[,] GetData(ManyVariable func, int minX, int maxX, int minY, int maxY, int pointCountX, int pointCountY)
+        {
+            double stepX = (double)(maxX - minX) / pointCountX;
+            double stepY = (double)(maxY - minY) / pointCountY;
+
+            double[] pointX = new double[pointCountX];
+            double[] pointY = new double[pointCountY];
+            for (int row = 0; row < pointCountX; row++)
+            {
+                pointX[row] = minX + (row * stepX);
+            }
+
+            for (int column = 0; column < pointCountY; column++)
+            {
+                pointY[column] = minY + (column * stepY);
+            }
+
+            double[,] data = new double[pointCountX, pointCountY];
+            for (int row = 0; row < pointCountY; row++)
+            {
+                for (int column = 0; column < pointCountX; column++)
+                {
+                    data[column, row] = func(new double[] { pointY[column], pointX[row] });
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Gets значение функции в точках x1,x2 для которых будет построена линия уровня.
+        /// </summary>
+        /// <param name="func">Функция двух переменных.</param>
+        /// <param name="minValue">Минимальное значение x1,x2.</param>
+        /// <param name="maxValue">Максимальное значение x1,x2.</param>
+        /// <param name="pointCount">Количество расчитываемых точек в диапазоне |xmax-xmin|.</param>
+        /// <returns>Значение функции в точках x1,x2.</returns>
+        private static double[,] GetData(ManyVariable func, int minValue, int maxValue, int pointCount)
+        {
+            return GetData(func, minValue, maxValue, minValue, maxValue, pointCount, pointCount);
+        }
+        #endregion
+
+        #region Structs
+
+        #endregion
+    }
+}
---
+++ branches/Optimization.VisualApplication/testFunctions.cs
@@ -0,0 +1,191 @@
+﻿namespace Optimization.VisualApplication
+{
+    using OptimizationMethods;
+    internal class TestFunctions
+    {
+        public int funcCount;
+        public ManyVariable[] funcs;
+        public double[][] startingPoint;
+        public double[][] exactSolution;
+        public int[] funcDimension;
+        public string[] funcsStr;
+
+        public TestFunctions()
+        {
+            funcCount = 11;
+            funcs = new ManyVariable[funcCount];
+            startingPoint = new double[funcCount][];
+            exactSolution = new double[funcCount][];
+            funcDimension = new int[funcCount];
+            funcsStr = new string[funcCount];
+            int funcNum;
+
+            ManyVariable function0 = delegate(double[] x)
+            {
+                return x[0] * x[0] * x[0] - x[0] * x[1] + x[1] * x[1] - 2 * x[0] + 3 * x[1] - 4;
+            };
+            funcNum = 0;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0;
+            startingPoint[funcNum][1] = 0;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 0.5;
+            exactSolution[funcNum][1] = -1.25;
+            funcs[funcNum] = function0;
+            funcsStr[funcNum] = "x[0] * x[0] * x[0] - x[0] * x[1] + x[1] * x[1] - 2 * x[0] + 3 * x[1] - 4";
+
+            ManyVariable function1 = delegate(double[] x)
+            {
+                return (x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0]);
+            };
+            funcNum = 1;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0;
+            startingPoint[funcNum][1] = 0;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 1;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function1;
+            funcsStr[funcNum] = "(x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0])";
+
+            ManyVariable function2 = delegate(double[] x)
+            {
+                return ((x[0] + 1) * (x[0] + 1) + x[0] * x[0]) * (x[0] * x[0] + (x[1] - 1) * (x[1] - 1));
+            };
+            funcNum = 2;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0.5;
+            startingPoint[funcNum][1] = 0;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 0;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function2;
+            funcsStr[funcNum] = "((x[0] + 1) * (x[0] + 1) + x[0] * x[0]) * (x[0] * x[0] + (x[1] - 1) * (x[1] - 1))";
+
+            ManyVariable function3 = delegate(double[] x)
+            {
+                return (x[1] * x[1] + x[0] * x[0] - 1) * (x[1] * x[1] + x[0] * x[0] - 1) +
+                    (x[0] + x[1] - 1) * (x[0] + x[1] - 1);
+            };
+            funcNum = 3;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0;
+            startingPoint[funcNum][1] = 3;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 0;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function3;
+            funcsStr[funcNum] = "(x[1] * x[1] + x[0] * x[0] - 1) * (x[1] * x[1] + x[0] * x[0] - 1) + (x[0] + x[1] - 1) * (x[0] + x[1] - 1)";
+
+            ManyVariable function4 = delegate(double[] x)
+            {
+                return -x[0] * x[0] * System.Math.Exp(1 - x[0] * x[0] - 20.25 * (x[0] - x[1]) * (x[0] - x[1]));
+            };
+            funcNum = 4;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0.1;
+            startingPoint[funcNum][1] = 0.5;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 1;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function4;
+            funcsStr[funcNum] = "-x[0] * x[0] * System.Math.Exp(1 - x[0] * x[0] - 20.25 * (x[0] - x[1]) * (x[0] - x[1]))";
+
+            ManyVariable function5 = delegate(double[] x)
+            {
+                return -x[0] * x[1] * System.Math.Exp(-(x[0] + x[1]));
+            };
+            funcNum = 5;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0;
+            startingPoint[funcNum][1] = 1;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 1;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function5;
+            funcsStr[funcNum] = "-x[0] * x[1] * System.Math.Exp(-(x[0] + x[1]))";
+
+            ManyVariable function6 = delegate(double[] x)
+            {
+                return 4 * (x[0] - 5) * (x[0] - 5) + (x[1] - 6) * (x[1] - 6);
+            };
+            funcNum = 6;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 8;
+            startingPoint[funcNum][1] = 9;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 5;
+            exactSolution[funcNum][1] = 6;
+            funcs[funcNum] = function6;
+            funcsStr[funcNum] = "4 * (x[0] - 5) * (x[0] - 5) + (x[1] - 6) * (x[1] - 6)";
+
+            ManyVariable function7 = delegate(double[] x)
+            {
+                return (x[0] * x[0] + x[1] - 11) * (x[0] * x[0] + x[1] - 11) + (x[0] + x[1] * x[1] - 7) * (x[0] + x[1] * x[1] - 7);
+            };
+            funcNum = 7;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0;
+            startingPoint[funcNum][1] = 0;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 3;
+            exactSolution[funcNum][1] = 2;
+            funcs[funcNum] = function7;
+            funcsStr[funcNum] = "(x[0] * x[0] + x[1] - 11) * (x[0] * x[0] + x[1] - 11) + (x[0] + x[1] * x[1] - 7) * (x[0] + x[1] * x[1] - 7)";
+
+            ManyVariable function8 = delegate(double[] x)
+            {
+                return 2 * x[0] * x[0] + x[0] * x[1] + x[1] * x[1];
+            };
+            funcNum = 8;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0.5;
+            startingPoint[funcNum][1] = 1;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 0;
+            exactSolution[funcNum][1] = 0;
+            funcs[funcNum] = function8;
+            funcsStr[funcNum] = "2 * x[0] * x[0] + x[0] * x[1] + x[1] * x[1]";
+
+            ManyVariable function9 = delegate(double[] x)
+            {
+                return x[0] * x[0] * x[0] + x[1] * x[1] - 3 * x[0] - 2 * x[1] + 1;
+            };
+            funcNum = 9;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0.5;
+            startingPoint[funcNum][1] = 1;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 1;
+            exactSolution[funcNum][1] = 1;
+            funcs[funcNum] = function9;
+            funcsStr[funcNum] = "x[0] * x[0] * x[0] + x[1] * x[1] - 3 * x[0] - 2 * x[1] + 1";
+
+            ManyVariable function10 = delegate(double[] x)
+            {
+                return x[0] * x[0] * x[0] * x[0] + x[1] * x[1] * x[1] * x[1] +
+                    2 * x[0] * x[0] * x[1] * x[1] - 4 * x[0] + 3;
+            };
+            funcNum = 10;
+            funcDimension[funcNum] = 2;
+            startingPoint[funcNum] = new double[funcDimension[funcNum]];
+            startingPoint[funcNum][0] = 0.5;
+            startingPoint[funcNum][1] = 1;
+            exactSolution[funcNum] = new double[funcDimension[funcNum]];
+            exactSolution[funcNum][0] = 1;
+            exactSolution[funcNum][1] = 0;
+            funcs[funcNum] = function10;
+            funcsStr[funcNum] = "x[0] * x[0] * x[0] * x[0] + x[1] * x[1] * x[1] * x[1] + 2 * x[0] * x[0] * x[1] * x[1] - 4 * x[0] + 3";
+        }
+    }
+}

bfa75e79d35d5c6750d25bb1f267e62f08551b88
plastiv <plastiv@aaa7b191-0527-1445-9cc7-a696b8423fba>
2010-01-13 22:21:47 +0000

    Тестовые функции теперь представлены по другом уалгоритму.

--- branches/Optimization.VisualApplication/Window1.xaml.cs
+++ branches/Optimization.VisualApplication/Window1.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Documents;
-using Microsoft.Research.DynamicDataDisplay.DataSources.MultiDimensional;
-using Microsoft.Research.DynamicDataDisplay.DataSources;
-using Microsoft.Research.DynamicDataDisplay.Common.Auxiliary;
-using Microsoft.Research.DynamicDataDisplay.Charts;
-using Microsoft.Research.DynamicDataDisplay;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Shapes;
 using Microsoft.Research.DynamicDataDisplay.Charts.Shapes;
+using Microsoft.Research.DynamicDataDisplay.Common.Auxiliary;
+using Microsoft.Research.DynamicDataDisplay.DataSources.MultiDimensional;
+using Microsoft.Research.DynamicDataDisplay.PointMarkers;
+using Optimization.Tests.Tasks;
 
 namespace Optimization.VisualApplication
 {
@@ -19,12 +13,11 @@ namespace Optimization.VisualApplication
     /// </summary>
     public partial class Window1 : Window
     {
-       readonly private int minValue;
-       readonly private int maxValue;
-       readonly private int pointCount;
+        readonly private int minValue;
+        readonly private int maxValue;
+        readonly private int pointCount;
 
-        TestFunctions mytest;
-        DataLayer myData;
+        LineSource lineSource;
         ViewportPolyline vwpolyline;
         int solPointIndex;
         int solPointCount;
@@ -32,37 +25,43 @@ namespace Optimization.VisualApplication
         public Window1()
         {
             InitializeComponent();
+
             minValue = Optimization.VisualApplication.Properties.Settings.Default.MinValue;
             maxValue = Optimization.VisualApplication.Properties.Settings.Default.MaxValue;
             pointCount = Optimization.VisualApplication.Properties.Settings.Default.PointCount;
 
             solPointCount = 0;
             solPointIndex = 0;
-            mytest = new TestFunctions();
-            myData = new DataLayer();
             vwpolyline = new ViewportPolyline();
             Loaded += new RoutedEventHandler(Window1_Loaded);
         }
 
         private void Window1_Loaded(object sender, RoutedEventArgs e)
         {
-            cmbMethods.Items.Add(DataLayer.Methods.Gradient);
-            cmbMethods.Items.Add(DataLayer.Methods.Hooke_Jeves);
+            cmbMethods.Items.Add(LineSource.Methods.Gradient);
+            cmbMethods.Items.Add(LineSource.Methods.Hooke_Jeves);
 
-            for (int i = 0; i < mytest.funcCount; i++)
-            {
-                cmbFunctions.Items.Add("Function " + i.ToString());
-            }
-
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask0());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask1());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask2());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask3());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask4());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask5());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask6());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask7());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask8());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask9());
+            cmbFunctions.Items.Add(new ManyVariableFunctionTask10());
         }
 
         private void cmbFunctions_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             plotter.Children.Remove(vwpolyline);
-            txtFunction.Text = mytest.funcsStr[cmbFunctions.SelectedIndex];
 
-            myData.Function = mytest.funcs[cmbFunctions.SelectedIndex];
-            WarpedDataSource2D<double> dataSource = DataSource.GetDataSource(myData.Function, minValue, maxValue, pointCount);
+            ManyVariableFunctionTask selectedTask = (ManyVariableFunctionTask)cmbFunctions.SelectedItem;
+            txtFunction.Text = selectedTask.expression;
+            lineSource = new LineSource(selectedTask.function);
+            WarpedDataSource2D<double> dataSource = DataSource.GetDataSource(selectedTask.function, minValue, maxValue, pointCount);
             isolineGraph.DataSource = dataSource;
             trackingGraph.DataSource = dataSource;
 
@@ -73,9 +72,17 @@ namespace Optimization.VisualApplication
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             plotter.Children.Remove(vwpolyline);
-            vwpolyline.Points = myData.GetSolutionPoints(cmbMethods.SelectedItem, new double[2] { double.Parse(txtX1.Text), double.Parse(txtX2.Text) });
-            solPointCount = myData.SolutionCount;
+            vwpolyline.Points = lineSource.GetPointCollection(cmbMethods.SelectedItem, new double[2] { double.Parse(txtX1.Text), double.Parse(txtX2.Text) });
+            solPointCount = lineSource.PointsCount;
             solPointIndex = solPointCount;
+            var marker = new CircleElementPointMarker
+            {
+                Size = 10,
+                Brush = Brushes.Red,
+                Fill = Brushes.Orange
+            };
+            marker.ToolTipText = "Value is";
+            vwpolyline.ToolTip = marker;
             plotter.AddChild(vwpolyline);
         }
 
@@ -86,21 +93,21 @@ namespace Optimization.VisualApplication
 
         private void btnForward_Click(object sender, RoutedEventArgs e)
         {
-            if(solPointIndex<solPointCount)
+            if (solPointIndex < solPointCount)
             {
-            vwpolyline.Points.Add(myData.GetCurrPoint(solPointIndex));
-            solPointIndex++;
+                vwpolyline.Points.Add(lineSource.GetCurrPoint(solPointIndex));
+                solPointIndex++;
             }
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
-            if (solPointIndex>0)
+            if (solPointIndex > 0)
             {
-                vwpolyline.Points.RemoveAt(solPointIndex-1);
+                vwpolyline.Points.RemoveAt(solPointIndex - 1);
                 solPointIndex--;
             }
-            
+
         }
 
         private void MenuItemIsoline_Click(object sender, RoutedEventArgs e)
@@ -108,6 +115,6 @@ namespace Optimization.VisualApplication
 
             MessageBox.Show("Menu item clicked");
 
-        } 
+        }
     }
 }
